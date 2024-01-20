442b2d1afcee1b62d0d57476a09b56b50f273925
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-01-20 18:58:25 +0500

    Start passwordManager

---
+++ PasswordManager/main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password manager")
+window.config(padx=20, pady=20)
+
+logo = PhotoImage(file="./logo.png")
+
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+canvas.create_image(100, 100, image=logo)
+
+window.mainloop()
