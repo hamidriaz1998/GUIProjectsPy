f0d1de1b62b3d5bd2773f6a9d108152ea7538e9d
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-05-30 10:14:21 +0500

    Complete password manager

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,8 +1,7 @@
 from tkinter import *
-
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
-
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+from tkinter import messagebox
+import random
+import pyperclip
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -27,8 +26,10 @@ passwordLabel.grid(column=0, row=3)
 # Entries
 websiteEntry = Entry(width=35)
 websiteEntry.grid(columnspan=2, column=1, row=1)
+websiteEntry.focus()
 emailEntry = Entry(width=35)
 emailEntry.grid(columnspan=2, column=1, row=2)
+emailEntry.insert(0, "example@ex.com")
 passwordEntry = Entry(width=21)
 passwordEntry.grid(column=1, row=3)
 # Buttons
@@ -37,4 +38,31 @@ generatePassButton.grid(column=2, row=3)
 addButton = Button(text="Add", width=36)
 addButton.grid(columnspan=2, column=1, row=4)
 
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    passwordEntry.delete(0, END)
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+    password = ''
+    password += ''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=nr_letters))
+    password += ''.join(random.choices('!@#$%^&*()', k=nr_symbols))
+    password += ''.join(random.choices('0123456789', k=nr_numbers))
+    passwordEntry.insert(0, password)
+    pyperclip.copy(password)
+generatePassButton.config(command=generate_password)
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    if len(websiteEntry.get()) == 0 or len(passwordEntry.get()) == 0 or len(emailEntry.get()) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+        return
+    is_ok = messagebox.askokcancel(title=websiteEntry.get(), message=f"These are the details entered: \nEmail: {emailEntry.get()}\nPassword: {passwordEntry.get()}\nIs it ok to save?")
+    if not is_ok:
+        return
+    with open("data.txt", "a") as file:
+        file.write(f"{websiteEntry.get()}\t{emailEntry.get()}\t{passwordEntry.get()}\n")
+    websiteEntry.delete(0, END)
+    passwordEntry.delete(0, END)
+addButton.config(command=save)
+# ---------------------------- MAIN LOOP ------------------------------- #
 window.mainloop()
